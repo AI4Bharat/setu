ed3db509d484896054af57f0d89cce3998e1d8c7
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2023-07-11 06:36:47 +0000

    Added line lengths, num lines and char distribution scripts

---
+++ document_level_filtering/line_lengths.py
@@ -0,0 +1,31 @@
+from typing import Dict
+from datasets import load_dataset, Dataset, IterableDataset
+import glob
+from indicnlp.tokenize import sentence_tokenize
+
+g = glob.glob("/mnt/data/nikhil/data/d1/*", recursive=True)
+
+line_lengths = dict()
+
+# length in words and characters and bytes
+
+
+def calc_line_lengths(a):
+    ll = dict()
+
+    ll["wc"] = len(a["body"].split())
+    ll["cc"] = len(a["body"])
+    ll["bc"] = len(str.encode(a["body"]))
+
+    line_lengths[a["title"]] = ll
+
+    return a
+
+
+dataset = load_dataset(
+    "json",
+    data_files={"en": g},
+)  # type: ignore
+
+iterable_ds = dataset["en"].map(calc_line_lengths)
+print(line_lengths)
